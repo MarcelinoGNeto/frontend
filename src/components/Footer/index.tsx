291dc4e82dcce661733cc11a3b0ed4456a2547e9
Marcelino Neto <marcelino.gneto@gmail.com>
2021-11-01 21:27:15 -0300

    frontend-mockado

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Marcelino Neto</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Para conhecer o código-fonte do projeto: <a href="https://github.com/MarcelinoGNeto" target="_blank" rel="noreferrer">Acesse meu GitHub</a></small></p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
